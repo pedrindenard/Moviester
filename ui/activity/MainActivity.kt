5fd397d6e6efb71935da2e6b1666927088b1e46c
pedrindenard <pedrominuzzi21@gmail.com>
2021-03-30 17:04:23 -0300

    Adicionando Camera

--- ui/activity/MainActivity.kt
+++ ui/activity/MainActivity.kt
@@ -2,20 +2,19 @@ package com.app.moviester.ui.activity
 
 import android.Manifest
 import android.app.Activity
-import android.content.Context
-import android.content.ContextWrapper
+import android.content.ContentValues
 import android.content.Intent
+import android.content.Intent.ACTION_PICK
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -25,15 +24,13 @@ import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_drawer.*
 import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.OutputStream
-import java.util.*
+
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var toggle: ActionBarDrawerToggle
+    lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,7 +46,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     // Configura o Menu Navigation
     private fun setupNavController() {
         val view: BottomNavigationView = findViewById(R.id.navigation_view)
-        val hostFragment = supportFragmentManager.findFragmentById(R.id.navigation_host_fragment) as NavHostFragment
+        val hostFragment =
+            supportFragmentManager.findFragmentById(R.id.navigation_host_fragment) as NavHostFragment
         val controller = hostFragment.navController
         view.setupWithNavController(controller)
     }
@@ -61,67 +59,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
-
-    // Funções exercidas dos botões
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_item_gallery ->
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
-                        PackageManager.PERMISSION_DENIED){
-                        val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                        requestPermissions(permissions, PERMISSION_CODE)
-                    }
-                    else{
-                        pickImageFromGallery()
-                    }
-                }
-                else{
-                    pickImageFromGallery()
-
-                }
-            R.id.nav_item_camera -> Toast.makeText(this, "Clicked item two", Toast.LENGTH_SHORT).show()
-            R.id.nav_item_config -> Toast.makeText(this, "Clicked item three", Toast.LENGTH_SHORT).show()
-        }
-        drawerLayout.closeDrawer(GravityCompat.START) // Fecha o Menu quano clicka em algum botão
-        return true
-    }
-
-    // Pega imagem da Galeria
-    private fun pickImageFromGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICK_CODE)
-    }
-
-    companion object {
-        private const val IMAGE_PICK_CODE = 1000
-        private const val PERMISSION_CODE = 1001
-    }
-
-    // Se tiver permissão pega a imagem da galeria, caso contrario, Toast "Permissão negada!"
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            PERMISSION_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED){
-                    pickImageFromGallery()
-                }
-                else{
-                    Toast.makeText(this, "Permissão negada!", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    // Coloca a imagem na Header_View do Menu lateral
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
-            nav_header_imageView.setImageURI(data?.data)
-        }
     }
 
     // Quando clicka no botão voltar do Android, ele fecha o Menu lateral e não o App
@@ -140,22 +77,119 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return super.onOptionsItemSelected(item)
     }
 
-    // Tentando achar possibilidades de usar isto
-    private fun saveImageToInternalStorage(drawableId:Int): Uri {
-        val drawable = ContextCompat.getDrawable(applicationContext,drawableId)
-        val bitmap = (drawable as BitmapDrawable).bitmap
-        val wrapper = ContextWrapper(applicationContext)
-        var file = wrapper.getDir("image/*", Context.MODE_PRIVATE)
-        file = File(file, "${UUID.randomUUID()}.jpg")
-
-        try {
-            val stream: OutputStream = FileOutputStream(file)
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-            stream.flush()
-            stream.close()
-        } catch (e: IOException){
-            e.printStackTrace()
+    // Funções exercidas dos botões
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_item_gallery -> buttonGallery()
+            R.id.nav_item_camera -> buttonCamera()
+            R.id.nav_item_config -> Toast.makeText(this, "Clicked item three", Toast.LENGTH_SHORT)
+                .show()
         }
-        return Uri.parse(file.absolutePath)
+        drawerLayout.closeDrawer(GravityCompat.START) // Fecha o Menu quano clicka em algum botão
+        return true
+    }
+
+    // Abre a caixinha de permissão do android - galeria
+    private fun buttonGallery() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_DENIED
+            ) {
+                val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                requestPermissions(permissions, PERMISSION_CODE_GALLERY)
+            } else {
+                pickImageFromGallery()
+            }
+        } else {
+            pickImageFromGallery()
+
+        }
+    }
+
+    // Abre a caixinha de permissão do android - camera
+    private fun buttonCamera() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED
+            ) {
+                val permission =
+                    arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                requestPermissions(permission, PERMISSION_CODE_GALLERY)
+            } else {
+                openCamera()
+            }
+        } else {
+            openCamera()
+        }
+    }
+
+    // Instancia a galeria
+    private fun pickImageFromGallery() {
+        val intent = Intent(ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    // Instancia a camera
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Nova foto")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Foto da camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    // Abre a camera ou galeria
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_CODE_GALLERY -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    pickImageFromGallery()
+                } else {
+                    Toast.makeText(this, "Permissão negada!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            PERMISSION_CODE_CAMERA -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    openCamera()
+                } else {
+                    Toast.makeText(this, "Permissão negada!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    // Coloca a imagem da galeria ou camera na ImagemView do Menu lateral
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            nav_header_imageView.setImageURI(data?.data)
+        } else {
+            if (resultCode == Activity.RESULT_OK) {
+                nav_header_imageView.setImageURI(image_uri)
+            }
+        }
+    }
+
+    // Constantes
+    companion object {
+        private const val IMAGE_PICK_CODE = 1000
+        private const val PERMISSION_CODE_GALLERY = 1001
+        private const val PERMISSION_CODE_CAMERA = 1000
+        private const val IMAGE_CAPTURE_CODE = 1001
+        var image_uri: Uri? = null
     }
 }
